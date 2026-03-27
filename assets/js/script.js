637d0db621162a49e03a86ae8b5bdf0747e58125
Sam Noble <samnoble@gmail.com>
2026-03-27 19:53:53 +0000

    Update carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,53 @@
-  // Preloader js    
-  $(window).on('load', function () {
-    $('.preloader').fadeOut(100);
+// Preloader js    
+$(window).on('load', function () {
+  $('.preloader').fadeOut(100);
+});
+
+(function ($) {
+  'use strict';
+
+  //  Search Form Open
+  $('#searchOpen').on('click', function () {
+    $('.search-wrapper').addClass('open');
+  });
+  $('#searchClose').on('click', function () {
+    $('.search-wrapper').removeClass('open');
   });
 
-  (function ($) {
-    'use strict';
-
-    //  Search Form Open
-    $('#searchOpen').on('click', function () {
-      $('.search-wrapper').addClass('open');
-    });
-    $('#searchClose').on('click', function () {
-      $('.search-wrapper').removeClass('open');
-    });
-
-    // featured post slider
-    const featuredPostSlider = $(".featured-post-slider");
-    featuredPostSlider.slick({
-      infinite: false,
-      adaptiveHeight: true,
-      arrows: false,
-      dots: true,
-      responsive: [{
-        breakpoint: 600,
-        settings: {
-          // default horizontal behavior
-        }
-      }]
-    });
-
-    featuredPostSlider.on('wheel', (function(e) {
-      var slickObj = $(this).slick('getSlick');
-      
-      if (e.originalEvent.deltaY > 0) {
-        if (slickObj.currentSlide < slickObj.slideCount - 1) {
-          e.preventDefault();
-          $(this).slick('slickNext');
-        }
-      } else {
-        if (slickObj.currentSlide > 0) {
-          e.preventDefault();
-          $(this).slick('slickPrev');
-        }
+  // featured post slider
+  const featuredPostSlider = $(".featured-post-slider");
+  featuredPostSlider.slick({
+    infinite: false,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+    dots: true,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        vertical: false,
+        verticalSwiping: false,
       }
-    }));
+    }]
+  });
 
-    // venobox initialize
-    $('.venobox').venobox();
+  featuredPostSlider.on('wheel', (function (e) {
+    var slickObj = $(this).slick('getSlick');
 
-  })(jQuery);
+    if (e.originalEvent.deltaY > 0) {
+      if (slickObj.currentSlide < slickObj.slideCount - 1) {
+        e.preventDefault();
+        $(this).slick('slickNext');
+      }
+    } else {
+      if (slickObj.currentSlide > 0) {
+        e.preventDefault();
+        $(this).slick('slickPrev');
+      }
+    }
+  }));
+
+  // venobox initialize
+  $('.venobox').venobox();
+
+})(jQuery);
